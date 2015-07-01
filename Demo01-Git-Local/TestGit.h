f5347deb691deee4187a4a43b41598c34ecfb6d9
tarena <tarena@gy.local>
2015-07-01 09:49:12 +0800

    2

---
+++ Demo01-Git-Local/TestGit.h
@@ -0,0 +1,13 @@
+//
+//  TestGit.h
+//  Demo01-Git-Local
+//
+//  Created by tarena on 15/7/1.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestGit : NSObject
+
+@end
---
+++ Demo01-Git-Local/TestGit.m
@@ -0,0 +1,14 @@
+//
+//  TestGit.m
+//  Demo01-Git-Local
+//
+//  Created by tarena on 15/7/1.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import "TestGit.h"
+#define a 2
+
+@implementation TestGit
+
+@end
